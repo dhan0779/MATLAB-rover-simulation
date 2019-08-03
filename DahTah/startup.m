580964ea9b01e0a878307e020f63dff6434a3279
dhan0779 <45704182+dhan0779@users.noreply.github.com>
2019-08-03 12:59:17 -0400

    Add startup files for PATH config

---
+++ DahTah/startup.m
@@ -0,0 +1,17 @@
+%startup 7.19.19
+
+baseWG = '\\files.brown.edu\home\dhan24\Documents\MATLAB' ;
+addpath \\files.brown.edu\home\dhan24\Documents\MATLAB\work\WonderfulGame
+addpath \\files.brown.edu\home\dhan24\Documents\MATLAB\work
+addpath \\files.brown.edu\home\dhan24\Documents\MATLAB\work\fold23
+
+bench
+
+sum(ans(1:4))
+
+pause(2)
+close
+close
+
+radi = 2.25;
+reso = 17;
